bab62e30d4911128b6a652ac712bc1cbdea3ac50
Renato Neto <skapemr@gmail.com>
2011-04-22 14:59:06 -0700

    Edited SimpleCache/example2.php via GitHub

--- SimpleCache/example2.php
+++ SimpleCache/example2.php
@@ -1,23 +1,23 @@
 <?php
 require('SimpleCache.php');
 
-$cache1 = new SimpleCache('/path/to/cache1/dir');
+$cache = new SimpleCache('/path/to/cache1/dir');
 
-$content = $cache1->read('cache1');
+$content = $cache->read('cache1');
 
 if(!$content) {
     $content = mt_rand(1, 99999999);
-    $cache1->save('cache1', $content, '1 minute');
+    $cache->save('cache1', $content, '1 minute');
 }
 
 echo 'Cache 1: ', $content, '<br />';
 
-$cache2 = new SimpleCache('/path/to/cache2/dir');
-$content = $cache2->read('cache2');
+$cache = new SimpleCache('/path/to/cache2/dir');
+$content = $cache->read('cache2');
 
 if(!$content) {
     $content = mt_rand(1, 99999999);
-    $cache2->save('cache2', $content, '2 minutes');
+    $cache->save('cache2', $content, '2 minutes');
 }
 
 echo 'Cache 2: ', $content;
